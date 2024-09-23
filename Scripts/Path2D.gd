7cc719a6edaeddd2ac515c3f2ab4a83b12939f4b
Dennisbobi <dennis.danskgamer@gmail.com>
2024-09-23 15:12:11 +0200

    ødelagt pis

--- Scripts/Path2D.gd
+++ Scripts/Path2D.gd
@@ -4,9 +4,10 @@ const VIRUS_1 = preload("res://Scenes/virus_1.tscn")
 @onready var virus_1_timer = $Virus1Timer
 
 
-
 func _ready():
 	virus_1_timer.start()
+	
+	wave([1], [9, 2], [0.1] [3.0])
 
 
 func _on_virus_1_timer_timeout():
@@ -15,3 +16,8 @@ func _on_virus_1_timer_timeout():
 	
 	virus_1_timer.start()
 	
+
+func wave(enemy : Array, amount : Array, TidMellem : Array):
+	
+	
+	pass
