ce43f8e2acc1a6b1de0237530d5e9614b6a4593d
Dennisbobi <dennis.danskgamer@gmail.com>
2024-09-23 14:58:47 +0200

    idk pls work

--- Scripts/Path2D.gd
+++ Scripts/Path2D.gd
@@ -1,7 +1,17 @@
 extends Path2D
 
 const VIRUS_1 = preload("res://Scenes/virus_1.tscn")
+@onready var virus_1_timer = $Virus1Timer
+
+
 
 func _ready():
+	virus_1_timer.start()
+
+
+func _on_virus_1_timer_timeout():
 	var virus = VIRUS_1.instantiate()
 	add_child(virus)
+	
+	virus_1_timer.start()
+	
