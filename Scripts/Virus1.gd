4992ed120ee2e7746126bffdfd7776cbf73caede
Dennisbobi <dennis.danskgamer@gmail.com>
2024-09-23 14:34:23 +0200

    simpelt follow

--- Scripts/Virus1.gd
+++ Scripts/Virus1.gd
@@ -1,1 +1,15 @@
-extends Node2D
+extends PathFollow2D
+
+var Path
+var speed = 0.1
+
+
+func _ready():
+	Path = get_parent()
+
+func _process(delta):
+	progress_ratio += speed * delta
+	
+	if progress_ratio >= 0.95:
+		queue_free()
+
