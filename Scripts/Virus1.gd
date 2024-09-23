94f05b9c92108e237c84caf4ba256b497b542b39
StorHage <davidfredsted@gmail.com>
2024-09-23 15:43:57 +0200

    tower arbejde, virus nu enemy

--- Scripts/Virus1.gd
+++ Scripts/Virus1.gd
@@ -6,10 +6,10 @@ var speed = 0.1
 
 func _ready():
 	Path = get_parent()
+	add_to_group("enemy")
 
 func _process(delta):
 	progress_ratio += speed * delta
 	
 	if progress_ratio >= 0.95:
 		queue_free()
-
---
+++ Scripts/attack.gd
@@ -0,0 +1,28 @@
+extends Area2D
+
+
+var enemies_inside = []  
+
+func _ready():
+	connect("body_entered", Callable(self, "_on_body_entered"))
+	connect("body_exited", Callable(self, "_on_body_exited"))
+
+func _on_body_entered(body):
+	if body.is_in_group("enemy"):  
+		enemies_inside.append(body)  
+		#print("Enemy entered: ", body.name)
+
+
+func _on_body_exited(body):
+	if body.is_in_group("enemy"):
+		enemies_inside.erase(body)  
+		#print("Enemy exited: ", body.name)
+
+#func _physics_process(_delta):
+	#for enemy in enemies_inside:
+		#var damage = 1
+		#bar.take_damage(damage)
+
+func _on_cooldown_timeout() -> void:
+	if enemy in enemies_inside:
+		var d = 1
