416228de915bc9f37f72bbfcca5093aa8ebdb48e
Dennisbobi <dennis.danskgamer@gmail.com>
2024-09-25 12:29:42 +0200

    start på hud

--- Scripts/Path2D.gd
+++ Scripts/Path2D.gd
@@ -2,12 +2,22 @@ extends Path2D
 
 const VIRUS_1 = preload("res://Scenes/virus_1.tscn")
 
+var currentRound = 1
+var waveDone = true
 
 func _ready():
-	wave([1, 1], [2, 10], [0.1, 1])
+	pass
+
+func _process(delta):
+	if currentRound == 1 and waveDone == true:
+		wave([1, 1], [2, 10], [0.1, 1])
+	elif currentRound == 2 and waveDone == true:
+		wave([1], [50], [0.1])
 
 
 func wave(enemy : Array, amount : Array, TidMellem : Array):
+	waveDone = false
+	
 	for i in enemy.size():
 		
 		for n in amount[i]:
@@ -22,3 +32,8 @@ func wave(enemy : Array, amount : Array, TidMellem : Array):
 			
 			call_deferred("add_child", virus)
 		
+	startWave()
+
+func startWave():
+	currentRound += 1
+	waveDone = true
