081fa3b5cda7c33275509e6aa55e17b9e8db9f0a
RazPalima <amilap.oettam@gmail.com>
2024-09-25 14:31:54 +0200

    ændring i main og hud

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -1,12 +1,21 @@
 extends Node2D
 
+const BASICTOWER = preload("res://Scenes/basic_tower.tscn")
+@onready var world = get_parent()
+
 var Health = 100
 var Currency = 50
 
 
-
+func _process(delta):
+	pass
 
 func damage(amount):
 	Health -= amount
 	print(Health)
 
+func _on_hud_buy_basic_tower():
+	var basicTower = BASICTOWER.instantiate()
+	world.call_deferred("add_child", basicTower)
+		
+	
--- Scripts/hud.gd
+++ Scripts/hud.gd
@@ -1,6 +1,7 @@
 extends CanvasLayer
 
 signal next_round
+signal buy_basic_tower
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,7 +14,7 @@ func _process(delta):
 
 
 func _on_basic_tower_buy_button_pressed():
-	pass
+	buy_basic_tower.emit()
 
 
 func _on_næste_runde_knap_pressed():
