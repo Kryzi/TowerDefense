4dafe84521e4e88e4ee9bdd43f4ef54ec0135843
Dennisbobi <dennis.danskgamer@gmail.com>
2024-10-02 13:32:51 +0200

    spawner enemies 2 og 3

--- Scripts/Path2D.gd
+++ Scripts/Path2D.gd
@@ -1,6 +1,8 @@
 extends Path2D
 
 const VIRUS_1 = preload("res://Scenes/virus_1.tscn")
+const VIRUS_2 = preload("res://Scenes/virus_2.tscn")
+const VIRUS_3 = preload("res://Scenes/virus_3.tscn")
 
 var currentRound = 0
 var waveDone = true
@@ -14,7 +16,11 @@ func _process(_delta):
 	elif currentRound == 2 and waveDone == true:
 		wave([1], [50], [0.1])
 	elif currentRound == 3 and waveDone == true:
-		wave([1, 1, 1], [3, 5, 10], [0.1, 0.3, 0.5])
+		wave([1, 2, 2], [3, 5, 10], [0.1, 0.3, 0.5])
+	elif currentRound == 3 and waveDone == true:
+		wave([3], [8], [0.5])
+	elif currentRound == 3 and waveDone == true:
+		wave([1, 2, 3], [3, 3, 3], [1, 1, 1])
 
 
 func wave(enemy : Array, amount : Array, TidMellem : Array):
@@ -30,7 +36,9 @@ func wave(enemy : Array, amount : Array, TidMellem : Array):
 			if enemy[i] == 1:
 				virus = VIRUS_1.instantiate()
 			elif enemy[i] == 2:
-				pass
+				virus = VIRUS_2.instantiate()
+			elif enemy[i] == 3:
+				virus = VIRUS_3.instantiate()
 			
 			call_deferred("add_child", virus)
 
