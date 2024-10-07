3887560ec105d9c8c83e0b77bb3d0f52ac1c7dc6
Kryzi <magnus.krydsfeldt@hotmail.com>
2024-10-07 12:54:44 +0200

    Tower 2

---
+++ Scripts/bullet_2.gd
@@ -0,0 +1,27 @@
+extends Area2D
+var damage = 1
+var travelled_distance = 0
+var pierce = 1
+
+func _physics_process(delta):
+	const SPEED = 200
+	const RANGE = 1200
+	
+	var direction = Vector2.UP.rotated(rotation)
+	position += direction * SPEED * delta
+	
+	travelled_distance += SPEED * delta
+	
+	if travelled_distance > RANGE:
+		queue_free()
+
+
+func _on_body_entered(body):
+	if body.name == "Tower":
+		return
+	else:
+		body.get_parent().takeDamage(damage)
+		pierce -= 1
+	
+	if pierce >= 0:
+		queue_free()
