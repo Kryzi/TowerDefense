2bfa35df6620a2d5e504cf13a4239eb59315b329
Dennisbobi <dennis.danskgamer@gmail.com>
2024-10-07 14:05:55 +0200

    kæmpe tower 3

--- Scripts/tower_3.gd
+++ Scripts/tower_3.gd
@@ -3,6 +3,7 @@ extends Area2D
 var placed = false
 var CurrentcyAmount = 20
 
+
 const FLOATINGTEXT = preload("res://Scenes/floatingtext.tscn")
 
 func popup():
@@ -11,18 +12,19 @@ func popup():
 	
 	text.get_node("Label").text = str(CurrentcyAmount)
 	
-	
 	var tween = get_tree().create_tween()
 	tween.tween_property(text, "position", %TextPos.global_position + _get_direction(), 0.75)
  
 	get_tree().current_scene.add_child(text)
  
 func _get_direction():
-	return Vector2(randf_range(-1, 1), -randf()) * 16
+	return Vector2(randf_range(-1, 1), -randf()) * 25
  
 func getMoney():
 	if placed == true:
+		$AnimatedSprite2D.play("MoneyGeterate")
 		get_node("/root/World/GameManager").Currency += CurrentcyAmount
+		
 		popup()
 		randomize()
 
